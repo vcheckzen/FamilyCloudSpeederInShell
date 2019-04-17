ba44bfe92d1241410ae2861d28b07952dfb917f2
vcheckzen <vcheckzen@outlook.com>
2019-04-17 21:35:12 +0800

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # FamilyCloudSpeederInShell
-[ 天翼家庭云提速 Shell 版 ] A Shell Implementation of FamilyCloudSpeeder, ESurfing.
+
+## Introduction
+
+A Shell Implementation of FamilyCloudSpeeder, ESurfing, which can be used in almost all linux platform.
+
+## How to Use
+
+### Download the Code
+
+Assuming you've installed git, then
+
+```bash
+git clone https://github.com/vcheckzen/XunleiKuainiaoInShell.git
+cd XunleiKuainiaoInShell
+```
+
+### Get Your Specific Parameters
+
+Follow [this](https://github.com/aiyijing/familycloudaccelerate/wiki/%E5%AE%B6%E5%BA%AD%E4%BA%91%E6%89%8B%E6%9C%BA%E7%AB%AF%E6%8A%93%E5%8C%85%E6%96%B9%E6%B3%95) and [this guide](https://github.com/aiyijing/familycloudaccelerate/issues/5), filling in the `config.json` file.
+
+### Run `speedup.sh` to Test
+
+```bash
+chmod +x speedup.sh
+./speedup.sh
+```
+
+# Appreciation
+
+- [familycloudaccelerate](https://github.com/aiyijing/familycloudaccelerate)
---
+++ speedup.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+base_dir=`dirname $0`
+source "$base_dir/utils.sh"
+config="$base_dir/config.json"
+
+
+session_key=`getSingleJsonValue "$config" "session_key"`
+session_secret=`getSingleJsonValue "$config" "session_secret"`
+method=`getSingleJsonValue "$config" "method"`
+rate=`getSingleJsonValue "$config" "rate"`
+UA=`getSingleJsonValue "$config" "User-Agent"`
+prodCode=`getSingleJsonValue "$config" "prodCode"`
+version=`getSingleJsonValue "$config" "version"`
+channelId=`getSingleJsonValue "$config" "channelId"`
+extra_header="User-Agent:$UA"
+
+
+ACCESS_URL="/family/qos/startQos.action"
+UP_QOS_URL="http://api.cloud.189.cn/family/qos/startQos.action"
+count=0
+echo "*******************************************"
+while :
+do
+    count=$((count+1))
+    echo "Sending heart_beat package <$count>"
+    date=`date -u '+%a, %d %b %Y %T GMT'`
+    data="SessionKey=$session_key&Operate=$method&RequestURI=$ACCESS_URL&Date=$date"
+    key="$session_secret"
+    signature=`hashHmac "sha1" "$data" "$key"`
+    split="~"
+    headers_string="SessionKey:$session_key"$split"Signature:$signature"$split"Date:$date"$split"$extra_header"
+    headers=`formatHeaderString "$split" "$headers_string"`
+    send_data="prodCode=$prodCode&version=$version&channelId=$channelId"
+    result=`post "$headers" "$UP_QOS_URL" "$send_data"`
+    echo "status_code:${result: -3}"
+    echo -e "response:\n${result:0:-3}"
+    [ "${result: -3}" = "400" ] &&  hint="succeeded" || hint="failed"
+    echo "Sending heart_beat package <$count> $hint"
+    echo "*******************************************"
+    sleep $rate
+done
