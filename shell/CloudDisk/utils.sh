6d7ea87d328bdfc447ee77a374e25a6383929dc3
LOGI <vcheckzen@outlook.com>
2019-05-22 15:32:39 +0800

    Fix sed error on macOS X

--- shell/CloudDisk/utils.sh
+++ shell/CloudDisk/utils.sh
@@ -32,7 +32,7 @@ post() {
 getSingleJsonValue() {
     FILE="$1"
     KEY="$2"
-    cat ${FILE} | grep "$KEY" | sed 's/,$//' | awk -F "[:]" '{ print $2 }' | sed 's/\( \|"\)//g'
+    cat ${FILE} | grep "$KEY" | sed 's/,$//' | awk -F "[:]" '{ print $2 }' | sed 's/ //g' | sed 's/"//g'
 }
 
 hashHmac() {
