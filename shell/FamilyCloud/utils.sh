ea992a79aebddbe926d42d7dec179e3ffa977833
LOGI <vcheckzen@outlook.com>
2019-05-22 15:36:40 +0800

    Fix sed error on macOS X

--- shell/FamilyCloud/utils.sh
+++ shell/FamilyCloud/utils.sh
@@ -26,13 +26,13 @@ post() {
     HEADER="$1"
     URL="$2"
     PAYLOAD="$3"
-    eval curl -s --connect-timeout "${CONNECTION_TIME}" -m "${TRANSMISSION_TIME}" -X POST "${URL}" "${HEADER}" -w "%{http_code}" -d "'$PAYLOAD'"
+    eval curl -s --connect-timeout "${CONNECTION_TIME}" -m "${TRANSMISSION_TIME}" -X POST "${URL}" "${HEADER}" -w %{http_code} -d "'$PAYLOAD'"
 }
 
 getSingleJsonValue() {
     FILE="$1"
     KEY="$2"
-    cat ${FILE} | grep "$KEY" | sed 's/,$//' | awk -F "[:]" '{ print $2 }' | sed 's/\( \|"\)//g'
+    cat ${FILE} | grep "$KEY" | sed 's/,$//' | awk -F "[:]" '{ print $2 }' | sed 's/ //g' | sed 's/"//g'
 }
 
 hashHmac() {
