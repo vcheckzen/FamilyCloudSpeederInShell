73256b6a3414181dc32d666c5eb9e233e0e91db0
vcheckzen <vcheckzen@outlook.com>
2019-05-23 00:14:01 +0800

    Add install script

--- install/router.sh
+++ install/router.sh
@@ -3,6 +3,19 @@
 padavan(){
     BASE_DIR='/etc/storage/'
     BOOT_FILE=${BASE_DIR}script/Sh99_cloudisk.sh
-    chmod +x ${BASE_DIR}CloudDisk/*.sh
-    echo -e "" > ${BOOT_FILE}
+    curl -o ${BOOT_FILE} https://raw.githubusercontent.com/vcheckzen/FamilyCloudSpeederInShell/assistantce/install/padavan/Sh99_cloudisk.sh
+    chmod +x ${BASE_DIR}CloudDisk/*.sh ${BOOT_FILE}
+    mtd_storage.sh save
+}
+
+merlin() {
+    BASE_DIR='/jffs/scripts/'
+    BOOT_FILE=${BASE_DIR}script/cloudisk_bootup.sh
+    curl -o ${BOOT_FILE} https://raw.githubusercontent.com/vcheckzen/FamilyCloudSpeederInShell/assistantce/install/merlin/cloudisk_bootup.sh
+    chmod +x ${BASE_DIR}CloudDisk/*.sh ${BOOT_FILE}
+}
+
+openwrt() {
+    chmod +x /root/CloudDisk/*.sh
+    sed '1i nohup /root/CloudDisk/speedup.sh > /dev/null 2>&1 &' -i /etc/rc.local
 }
